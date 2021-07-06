7c8344fd2f86a9466a4cd419eedc67ead3aed466
Leninner <mazabandalenin180@gmail.com>
2021-07-06 15:14:59 -0500

    Añado cabecera solo con Javascript y además soy capaz de conectar estilos con HTML desde Javascript

--- src/home.js
+++ src/home.js
@@ -1,15 +1,50 @@
 const contenedor = document.querySelector("#content");
 
-function doHeader() {
-    let div = document.createElement("div"),
-        divHijo = document.createElement("div");
+function doHeader(icon) {
+    let header = document.createElement("div"),
+        topTitle = document.createElement("div"),
+        topMenu = document.createElement("div"),
+        img = new Image(),
+        hTitle = document.createElement("div"),
+        hOne = document.createElement("h1"),
+        pOne = document.createElement("p"),
+        lista = document.createElement("ul"),
+        itemOne = document.createElement("li"),
+        itemTwo = document.createElement("li"),
+        itemThree = document.createElement("li"),
+        refItemOne = document.createElement("a"),
+        refItemTwo = document.createElement("a"),
+        refItemThree = document.createElement("a");
 
-    divHijo.textContent = "Leninnn";
+    //Añadir propiedades
 
-    div.classList.add("hello");
+    img.src = icon;
+    img.alt = "Comidita";
+    hOne.textContent = "Cheda";
+    pOne.textContent = "Comida Tradicional";
+    refItemOne.textContent = "Menú";
+    refItemOne.href = "#menu";
+    refItemTwo.textContent = "Reservaciones";
+    refItemTwo.href = "#reservation";
+    refItemThree.textContent = "Contacto";
+    refItemThree.href = "#contact";
 
-    div.appendChild(divHijo);
-    contenedor.appendChild(div);
+    // Añadir Estilos
+
+    header.classList.add("header");
+    topTitle.classList.add("topTitle");
+    topMenu.classList.add("topMenu");
+
+    // Añadir hijos
+    itemOne.appendChild(refItemOne);
+    itemTwo.appendChild(refItemTwo);
+    itemThree.appendChild(refItemThree);
+    lista.append(itemOne, itemTwo, itemThree);
+    hTitle.append(hOne, pOne);
+    topTitle.append(img, hTitle);
+    topMenu.appendChild(lista);
+    header.append(topTitle, topMenu);
+    contenedor.appendChild(header);
 }
 
 export default doHeader;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import doHeader from "./home.js";
 import "./style.css";
+import icon from "./Images/icon.png";
 
-doHeader();
+doHeader(icon);
